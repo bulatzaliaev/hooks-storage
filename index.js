df6246f54661349a397f65d1e05ab2d7fd841f86
Bulat <bulat87@gmail.com>
2020-05-19 01:01:00 +0300

    useAsync v0.0.2

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+
+const useAsync = (asyncFunction, immediate = true) => {
+    const [pending, setPending] = useState(false);
+    const [value, setValue] = useState(null);
+    const [error, setError] = useState(null);
+
+    const callAsync = useCallback(() => {
+        setPending(true);
+        setValue(null);
+        setError(null);
+
+        return asyncFunction()
+            .then(response => setValue(response))
+            .catch(error => setError(error))
+            .finally(() => setPending(false));
+    }, [asyncFunction]);
+
+    useEffect(() => {
+        if (immediate) {
+            callAsync();
+        }
+    }, [callAsync, immediate]);
+
+    return { callAsync, pending, value, error };
+};
 
 function useOnClickOutside(ref, handler) {
     useEffect(
@@ -24,5 +49,6 @@ function useOnClickOutside(ref, handler) {
 }
 
 module.exports = {
-    useOnClickOutside
+    useAsync,
+    useOnClickOutside,
 };
